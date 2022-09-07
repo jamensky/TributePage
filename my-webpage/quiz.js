3da41e2c955f4857f34fcaea582354f4560c2014
Jamal <jamensky1991@gmail.com>
2022-09-07 19:47:38 +0100

    separated quiz logic in js file

--- my-webpage/quiz.js
+++ my-webpage/quiz.js
@@ -1,3 +1,7 @@
-document.getElementById("submit").onclick = function(){
-    var oscars = document.getElementById()
+function tot_oscars(n){
+    if (n == 1){
+      document.write("Your answer is" + n.value) 
+    } else {
+      document.write("Not quite the right answer... try again!")
+    }
 }
