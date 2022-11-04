26bc206bea4bf31312eeaf7161cb2eb7de482517
Jamal <jamensky1991@gmail.com>
2022-11-04 13:29:13 +0000

    added time and date

--- my-webpage/quiz.js
+++
@@ -1,9 +0,0 @@
-
-function tot_oscars(n_of_oscars){
-
-    if (n_of_oscars.value == 1){
-      document.write("That's right buddy!! He only won" + n_of_oscars.value) 
-    } else {
-      document.write("Not quite the right answer... try again!")
-    }
-}
